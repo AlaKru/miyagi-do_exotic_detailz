5d1c1b75de170ba4ec1be377e7d892bcb9f26244
AlaKru <AKrupp2@gmail.com>
2020-11-19 17:47:15 -0500

    Added Javascript img gallery, and html2 for Appointments

--- script/script.js
+++ script/script.js
@@ -1,6 +1,24 @@
-console.log("Hello");
-function totalHeightCent (feet, inches)
-    return (feet * 12) + inches * 2.65;
+var i = 0; //Start Point
+var images = [];
+var time = 3000;
 
-console.log (totalHeightCent);
+// Image List
+images[0] = 'img/pexels-adrian-dorobantu-2127733.jpg';
+images[1] = 'img/pexels-jae-park-3802510.jpg';
+images[2] = 'img/pexels-pixabay-326259.jpg';
+images[3] = 'img/pexels-yurii-hlei-1545743.jpg';
 
+// Change Img
+function changeImg(){
+    document.slide.src = images[i];
+
+    if(i < images.length - 1){
+        i++;
+    } else {
+        i = 0;
+    }
+
+    setTimeout("changeImg()", time);
+}
+
+window.onload = changeImg;
